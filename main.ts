2cfb55b2a01a9f3167a9e5521cbea824ae6c382c
Azlankhan12345 <161835064+Azlankhan12345@users.noreply.github.com>
2024-04-02 14:12:51 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+
+// User variable
+let todos: string[] = [];
+async function main() {
+    while (true) {
+        let input = await inquirer.prompt([
+            {
+                name: "Todoitem",
+                type: "input",
+                message: 'Add a todo item to the list'
+            },
+            {
+                name: "addMore",
+                type: "list",
+                message: "Do you want to add more items?",
+                choices: ["yes", "no"]
+            }
+        ]);
+
+        // Output of program
+        const { Todoitem, addMore } = input;
+
+        todos.push(Todoitem);
+        if (addMore === "no") {
+            console.log("TodoList:");
+
+            // Displaying todo items
+            for (let i = 0; i < todos.length; i++) {
+                console.log(todos[i]);
+            }
+
+            break; // Exit the loop when the user doesn't want to add more
+        }
+    }
+}
+
+main();
